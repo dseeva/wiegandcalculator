c389dff3cbd1c2ed3d7308ed41b3348ce6b27a96
dseeva <30951460+dseeva@users.noreply.github.com>
2021-03-13 11:39:36 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+# Solution 6000 Wiegand Calculator
+
+from art import *
+import numpy as np
+
+art = text2art("Wiegand Calculator")
+
+
+def mci():
+    sc = hex(int(input("Enter your Site code: ")))[2:]
+    cn = int(input("Enter first card in your card range: "))
+    num = int(input("Enter the amount of cards you would like to iterate: "))
+    print(" ")
+    endcn = int(cn + num)
+    array = []
+    for x in np.arange(cn, endcn, 1):
+        array = np.append(array, x)
+    vector = np.vectorize(int)
+    array = vector(array)
+    for x in array:
+        cn = hex(x)[2:]
+        if len(cn) <= 1:
+            cn = str("000") + str(cn)
+        if len(cn) <= 2:
+            cn = str("00") + str(cn)
+        if len(cn) <= 3:
+            cn = str("0") + str(cn)
+        if len(sc) <= 1:
+            sc = str("0") + str(sc)
+        output = "0000" + sc + cn
+        print(output)
+    print(" ")
+    menu1 = input("Type 1 to return to main menu or 2 to create another: ")
+    if menu1 == str(1):
+        choice()
+    elif menu1 == str(2):
+        mci()
+
+def sci():
+    while True:
+        sc = hex(int(input("Enter your Site code: ")))[2:]
+        cn = hex(int(input("Enter your card number: ")))[2:]
+
+        sc_len = len(sc)
+        if sc_len <= 1:
+            sc = str("0") + str(sc)
+        else:
+            pass
+
+        if len(cn) <= 1:
+            cn = str("000") + str(cn)
+        if len(cn) <= 2:
+            cn = str("00") + str(cn)
+        elif len(cn) <= 3:
+            cn = str("0") + str(cn)
+
+        output = "0000" + sc + cn
+        print(" ")
+        print("Combined output for input into database: ")
+        print(output)
+        print(" ")
+        menu1 = input("Type 1 to return to main menu or 2 to create another: ")
+        if menu1 == str(1):
+            choice()
+        elif menu1 == str(2):
+            sci()
+
+def choice() -> object:
+    print(art)
+    print("Please select from these options:")
+    print("1. Create user data from range of card numbers")
+    print("2. Create user data from single card number")
+    print("(P.S. Enter any other number to exit)")
+    menu = int(input("Enter: "))
+
+    if menu == 1:
+        mci()
+        exit()
+    elif menu == 2:
+        sci()
+        exit()
+    else:
+        exit()
+
+
+if __name__ == "__main__":
+    choice = choice()
